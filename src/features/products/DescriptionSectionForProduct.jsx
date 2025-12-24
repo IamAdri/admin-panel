924a89e4ee1e2af5890367f6f72ee913d16e9e3e
IamAdri <atoma304@gmail.com>
2025-12-24 15:17:44 +0200

    Add to display show more button only if text is too long in description

--- src/features/products/DescriptionSectionForProduct.jsx
+++ src/features/products/DescriptionSectionForProduct.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import Button from "../../ui/Button";
 import ModalLayout from "../../ui/ModalLayout";
 import CloseButton from "../../ui/CloseButton";
+import { useEffect, useRef, useState } from "react";
 
 const DescriptionDiv = styled.div`
   padding: 3px;
@@ -11,7 +12,7 @@ const DescriptionDiv = styled.div`
   width: 500px;
 `;
 const DescriptionText = styled.p`
-  width: 80%;
+  width: 400px;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
@@ -22,9 +23,19 @@ const DescriptionTextModal = styled.p`
 `;
 
 function DescriptionSectionForProduct({ product }) {
+  const descriptionRef = useRef(null);
+  const [isButtonRefMounted, setIsButtonRefMounted] = useState(false);
+  useEffect(() => {
+    if (descriptionRef.current.offsetWidth < descriptionRef.current.scrollWidth)
+      setIsButtonRefMounted(true);
+  }, [descriptionRef]);
   //Opne/close description modal
   const handleOpenDescription = (e) => {
     const description = e.target.nextSibling;
+    console.log(
+      descriptionRef.current.offsetWidth < descriptionRef.current.scrollWidth
+    );
+    // console.log(descriptionRef.current.style.overflow === "hidden");
     description.style.display = "flex";
   };
   const handleCloseDescription = (e) => {
@@ -33,10 +44,14 @@ function DescriptionSectionForProduct({ product }) {
   };
   return (
     <DescriptionDiv>
-      <DescriptionText>{product.description}</DescriptionText>
-      <Button $type="tertiary" onClick={handleOpenDescription}>
-        Show more
-      </Button>
+      <DescriptionText ref={descriptionRef}>
+        {product.description}
+      </DescriptionText>
+      {isButtonRefMounted && (
+        <Button $type="tertiary" onClick={handleOpenDescription}>
+          Show more
+        </Button>
+      )}
       <ModalLayout>
         <CloseButton handleCloseModal={handleCloseDescription} />
         <DescriptionTextModal>{product.description}</DescriptionTextModal>
