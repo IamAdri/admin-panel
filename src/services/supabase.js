c668edc1b82af3576df7d276a42499b301f770bd
IamAdri <atoma304@gmail.com>
2025-12-28 19:18:35 +0200

    Add sensitive data in .env file

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://ydghorluedeqhuwnokqn.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkZ2hvcmx1ZWRlcWh1d25va3FuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA3NjAxOTMsImV4cCI6MjA2NjMzNjE5M30.eeR-MGRRZivlr7KJD2KT1RxgJqzl7UY46SXcoxlrMHU";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
